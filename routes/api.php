845c654fe0e7476e12ca168b8f7b31694e8ba58d
chinch2 <johao5991@gmail.com>
2021-03-22 20:45:08 -0400

    Final changes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts', function() {
+    $posts = App\Models\Post::all();
+
+    return $posts;
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
